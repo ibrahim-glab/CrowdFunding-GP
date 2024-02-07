1138e78557f8ec76be5f65371b78e97601931b1c
ibrahim x <ibrahim20191700003@cis.asu.edu.eg>
2024-02-07 05:47:41 +0200

    update Repo

---
+++ BlockChainWithBackEnd/contracts/BaseCampaign.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract BaseCampaign {
+    address public Owner;
+    uint256 public minimumcontribution;
+    uint256 public goal;
+    mapping(address => uint256) public contributors;
+    address[] public contributorsList;
+    uint256 public totalContributions;
+    uint256 public campaignEndTime;
+    bool private authorized ; 
+    enum CampaignStatus {
+        Active,
+        Successful,
+        Pending,
+        Failed
+    }
+    CampaignStatus public campaignStatus;
+    event ContributionReceived(
+        address indexed contributor,
+        uint256 amount,
+        string message
+    );
+    struct Request {
+        string description;
+        uint256 value;
+        address payable recipient;
+        bool complete;
+        uint256 approvalCount;
+    }
+    Request[] public requests;
+
+    constructor(
+        address owner,
+        uint256 minimumContribution,
+        uint256 durationInDays,
+        uint256 Goal
+    ) {
+        Owner = owner;
+        minimumcontribution = minimumContribution;
+        campaignEndTime = block.timestamp + (durationInDays * 1 days);
+        campaignStatus = CampaignStatus.Pending;
+        goal = Goal;
+    }
+
+    modifier restricted() {
+        require(msg.sender == Owner, "Only the Owner can call this function");
+        _;
+    }
+
+    function isCampaignActive() public view returns (bool) {
+        if (campaignStatus == CampaignStatus.Active) {
+            return true;
+        }
+        return false;
+    }
+
+    function contribute(string memory message) public payable {
+        require(block.timestamp < campaignEndTime, "Campaign has ended");
+        require(
+            msg.value >= minimumcontribution,
+            "Contribution amount too low"
+        );
+
+        contributors[msg.sender] += msg.value;
+        totalContributions += msg.value;
+        contributorsList.push(msg.sender);
+        emit ContributionReceived(msg.sender, msg.value, message);
+    }
+
+    // function createRequest(
+    //     string memory description,
+    //     uint256 value,
+    //     address payable recipient
+    // ) public restricted {
+    //     Request memory newRequest = Request({
+    //         description: description,
+    //         value: value,
+    //         recipient: recipient,
+    //         complete: false,
+    //         approvalCount: 0
+    //     });
+    //     requests.push(newRequest);
+    // }
+
+    //   function approveRequest(uint256 index) public {
+    //     Request storage request = requests[index];
+    //     require(contributors[msg.sender] > 0, "You must be a contributor to approve");
+    //     require(!request.approvals[msg.sender], "You can't approve the same request twice");
+
+    //     request.approvals[msg.sender] = true;
+    //     request.approvalCount++;
+    // }
+
+    function endCampaign() public restricted {
+        require(
+            block.timestamp >= campaignEndTime,
+            "Campaign has not ended yet"
+        );
+        if (totalContributions < goal) {
+            campaignStatus = CampaignStatus.Failed;
+            refundContributors();
+        } else if (totalContributions >= goal) {
+            campaignStatus = CampaignStatus.Successful;
+        }
+    }
+
+    function refundContributors() public {
+        for (uint256 i = 0; i < contributorsList.length; i++) {
+            payable(contributorsList[i]).transfer(
+                contributors[contributorsList[i]]
+            );
+        }
+    }
+}
--- BlockChainWithBackEnd/contracts/CampaginFactory.sol
+++ BlockChainWithBackEnd/contracts/CampaginFactory.sol
@@ -1,16 +1,48 @@
 // SPDX-License-Identifier: GPL-3.0
-import "BlockCahin/contracts/Campagin.sol";
+import "./CharityBasedCampaign.sol";
+import "./EquaityBasedCampaign.sol";
+import "./rewardBasedCampaign.sol";
+import "./BaseCampaign.sol";
 
 pragma solidity >=0.7.0 <0.9.0;
-contract CapmpaginFactory{
-address[] public deployedProjects;
 
-    function createProject(uint256 minimumContribution) public {
-        address newProject = address(new Campagin(minimumContribution , msg.sender));
-        deployedProjects.push(newProject);
+contract CapmpaginFactory {
+    BaseCampaign[] public deployedProjects;
+    enum CampaignType {
+        Charity,
+        Equaity,
+        Reward
     }
 
-    function getDeployedProjects() public view returns (address[] memory) {
+    function createProject(
+        uint256 minimumContribution,
+        uint256 durationInDays,
+        uint256 Goal,
+        CampaignType campType
+    ) public {
+        if(campType == CampaignType.Charity){
+             BaseCampaign newCamp = new CharityBasedCampaign(
+            msg.sender,
+            minimumContribution,durationInDays , Goal);
+            deployedProjects.push(newCamp);
+        }
+        else if(campType == CampaignType.Equaity){
+            BaseCampaign newCamp = new EquaityBasedCampaign(
+            msg.sender,
+            minimumContribution,durationInDays , Goal);
+            deployedProjects.push(newCamp);
+        }
+        else if(campType == CampaignType.Reward){
+            BaseCampaign newCamp = new RewardBasedCampaign(
+            msg.sender,
+            minimumContribution,durationInDays , Goal);
+            deployedProjects.push(newCamp);
+        }
+       
+        
+    }
+
+    function getDeployedProjects() public view returns (BaseCampaign[] memory) {
         return deployedProjects;
     }
 }
