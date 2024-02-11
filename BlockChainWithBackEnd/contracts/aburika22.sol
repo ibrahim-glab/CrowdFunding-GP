9f233eac3d732d4a84d186f059f0792bc3bbc7c5
ibrahim x <ibrahim20191700003@cis.asu.edu.eg>
2024-02-11 16:20:42 +0200

    update Repo Ibrahim 9/2 8.30 am

--- BlockChainWithBackEnd/contracts/aburika22.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
-contract AbuTrika{
-    address immutable  adminAddress;
-    constructor(address _adminAddress){
-        adminAddress = _adminAddress;
-
-    }
-}
