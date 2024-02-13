514f291dc5b7bb8ca3f2ebb274329da3b2559895
abdelrhman <abdelrhmankhaled537@gmail.com>
2024-02-13 21:37:05 +0200

    adding fundcards

--- websiteV1/Crowdfunding_blockChain system/src/App.jsx
+++ websiteV1/Crowdfunding_blockChain system/src/App.jsx
@@ -1,8 +1,10 @@
-
+import ocean from './assets/ocean.png';
 import './index.css';  
 import "./App.css";
 import SideBar from "./Components/SideBar";
 import NavBar from  "./Components/NavBar"
+import FundCard from './Components/FundCard';
+import { dummyData } from './test';
 function App() {
   return (
     <>
@@ -13,7 +15,15 @@ function App() {
         </div>
         <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         <NavBar />
+        <div className="flex flex-wrap mt-[20px] gap-[26px]">
+          {dummyData.map((data,index)=>{
+            return(
+              <FundCard key={index} {...data} />
+            )
+          })}
         </div>
+        </div>
+     
       </div>
     </>
   );
