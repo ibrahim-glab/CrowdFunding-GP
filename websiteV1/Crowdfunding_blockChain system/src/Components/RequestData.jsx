b47c46e277c4bfe7fa69790de176451df02cc043
abdelrhman <abdelrhmankhaled537@gmail.com>
2024-02-15 17:45:33 +0200

    adding Amr's components and working on routing

---
+++ websiteV1/Crowdfunding_blockChain system/src/Components/RequestData.jsx
@@ -0,0 +1,12 @@
+function RequestData({ walletID, title, date, amount, status, index }) {
+    return (
+        <tr className={`data-row ${index % 2 === 0 ? 'even-row' : 'odd-row'}`}>
+            <td>{walletID}</td>
+            <td><a href="#">{title}</a></td>
+            <td>{date}</td>
+            <td>{amount}</td>
+            <td>{status}</td>
+        </tr>
+    )
+}
+export default RequestData;
