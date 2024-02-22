607c9ae1566ad2447288a7e383e24d04fe9ceaf0
ibrahim x <ibrahim20191700003@cis.asu.edu.eg>
2024-02-22 06:34:41 +0200

    UPDATE Create and display data in Camp Component

--- websiteV1/src/Components/dashboard/Campaigns.jsx
+++ websiteV1/src/Components/dashboard/Campaigns.jsx
@@ -6,6 +6,7 @@ import { BasecontractABI, contractABI } from "../../constants";
 import { v4 as uuidv4 } from "uuid";
 import FundCard from './FundCard';
 import { loader } from '../../assets';
+import { ethers } from 'ethers';
 
 const Campaigns = ({ title, isLoading, campaigns }) => {
   //   const navigate = useNavigate();
@@ -24,7 +25,42 @@ const Campaigns = ({ title, isLoading, campaigns }) => {
 console.log(data);
 /// show addresses for all  campigns
 
+   //Base Contract Example
+    //0x2bF80d2B43084b48C7E96cC48bCe781EA1Bd7777
+    const { contract: Basecontract } = useContract( "0x2bF80d2B43084b48C7E96cC48bCe781EA1Bd7777", BasecontractABI);
+    console.log(Basecontract);
+    //Display Owner
+    const {data: data1,  isLoading2, error2} = useContractRead(
+      Basecontract,
+      "Owner",
+    );
+    console.log("Owner is ", data1);
+    // Display Title of each campaign
+    const {data: data2,  isLoading3, error3} = useContractRead(
+      Basecontract,
+      "title",
+    );
+    console.log("Title is ", data2);
 
+    // Display Description of each campaign
+    const {data: data3,  isLoading4, error4} = useContractRead(
+      Basecontract,
+      "description",
+    );
+    console.log("Description is ", data3);
+
+    // goal of each campaign
+    // Display goal of each campaign
+    const {data: data4,  isLoading5, error5} = useContractRead(
+      Basecontract,
+      "goal",
+    );
+ // Check if data4 is defined before using it
+ if (data4 !== undefined) {
+  console.log("Goal is ", ethers.utils.formatEther(data4));
+} else {
+  console.log("Goal data is not available yet");
+}
   return (
     <div>
       <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
